be8a25b66f90d7240584ed0023f8cef49cb0c50e
jc428029 <lucas.clowes@my.jcu.edu.au>
2018-03-14 11:50:08 +1000

    add name printing

--- oddName.py
+++ oddName.py
@@ -1,1 +1,14 @@
 """Lucas Clowes"""
+
+
+def main():
+    name = input("Enter your name: ")
+    while not name:
+        print('Entry must not be blank')
+        name = input("Enter your name: ")
+    length = len(name)
+    for i in range(1, length, 2):
+        print(name[i])
+
+
+main()
